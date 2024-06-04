e8ed1ef9072ac2b773c87c2c5f9802f0c690a453
iqbalmungid <iqbalthea3@gmail.com>
2024-06-04 09:32:40 +0700

    home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+  const sections = ["home", "projects", "journey", "about"];
+  let currentSectionIndex = 0;
+
+  function toggleFabFirst() {
+    if (currentSectionIndex === 0) {
+      $("#fab-up").hide();
+    } else {
+      $("#fab-up").show();
+    }
+  }
+
+  function toggleFabLast() {
+    if (currentSectionIndex === sections.indexOf("about")) {
+      $("#fab-down").hide();
+    } else {
+      $("#fab-down").show();
+    }
+  }
+
   $(".nav-link").click(function (event) {
     event.preventDefault();
     var page = $(this).data("page");
@@ -6,6 +25,31 @@ $(document).ready(function () {
     $("#" + page).addClass("active");
     $(".nav-link").removeClass("active");
     $(this).addClass("active");
+    toggleFabFirst();
+    toggleFabLast();
+  });
+
+  $("#fab-down").click(function () {
+    currentSectionIndex = (currentSectionIndex + 1) % sections.length;
+    const nextSection = sections[currentSectionIndex];
+    $(".content div").removeClass("active");
+    $("#" + nextSection).addClass("active");
+    $(".nav-link").removeClass("active");
+    $(`.nav-link[data-page='${nextSection}']`).addClass("active");
+    toggleFabFirst();
+    toggleFabLast();
+  });
+
+  $("#fab-up").click(function () {
+    currentSectionIndex =
+      currentSectionIndex === 0 ? sections.length - 1 : currentSectionIndex - 1;
+    const prevSection = sections[currentSectionIndex];
+    $(".content div").removeClass("active");
+    $("#" + prevSection).addClass("active");
+    $(".nav-link").removeClass("active");
+    $(`.nav-link[data-page='${prevSection}']`).addClass("active");
+    toggleFabFirst();
+    toggleFabLast();
   });
 
   $('[data-bs-toggle="tooltip"]').tooltip();
